81b246743be18f574d0c4dcf36bab4e72336131f
ppoffice <ppoffice_2008@163.com>
2015-07-11 21:59:49 +0800

    introduce gulp

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename');
+ 
+gulp.task('compress', function() {
+  return gulp.src('src/validator.js')
+    .pipe(uglify({mangle:true}))
+    .pipe(rename('validator.min.js'))
+    .pipe(gulp.dest('dist'));
+});
