c49ee14422da023c7f17a07d2973868797a62a87
ppoffice <ppoffice_2008@163.com>
2015-07-26 12:30:54 +0800

    update package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename');
- 
+
 gulp.task('compress', function() {
   return gulp.src('src/validator.js')
     .pipe(uglify({mangle:true}))
     .pipe(rename('validator.min.js'))
     .pipe(gulp.dest('dist'));
 });
+
+gulp.task('copy', function () {
+  return gulp.src('src/validator.js')
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['compress', 'copy']);
